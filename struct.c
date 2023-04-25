f51b7c4be3b32d468ce83ae2a65d7134d300e860
michaelningombam <124857302+michaelningombam@users.noreply.github.com>
2023-04-25 16:03:57 +0530

    Update struct.c

--- struct.c
+++ struct.c
@@ -1,33 +1,27 @@
-#include <windows.h>
-
-#include<stdio.h>
-#include<stdlib.h>
-
+ #include<stdio.h>
 struct data
 {
-	char name[50];
-	char address[50];
-	int age;
-	
+    char name[50];
+    char address[50];int age;
 };
-
 int main()
 {
-	int i;
-	char a[50];
-	char add[50];
-	int ag;
-	struct data students[5];
-	
-	for(i=1;i<=5;i++)
-	{
-		printf("Please enter name of %d Student",i);
-		scanf("%s",&students[i].name);
-		printf("Please enter address of %d Student",i);
-		scanf("%s",&students[i].address);
-		printf("Please enter age of %d student",i);
-		scanf("%d",&ag);
-		students[i].age=("%d",ag);
-	}
-	return 0;
+    int i;
+    struct data students[5];
+    for(i=0;i<5;i++)
+    {
+        printf("Enter name of student %d    ",i+1);
+        scanf("%s",&students[i].name);
+        printf("Enter address of student %d    ",i+1);
+        scanf("%s",&students[i].address);
+        printf("Enter age of student %d    ",i+1);
+        scanf("%d",&students[i].age);
+        
+    }
+    printf("Name\tAddress\tAge\n");
+    for(i=0;i<5;i++)
+    {
+    printf("%s\t%s\t%d\n",students[i].name,students[i].address,students[i].age);
+    }
+    
 }
